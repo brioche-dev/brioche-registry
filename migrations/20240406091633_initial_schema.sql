4ca55bb5a99161f9208909a9410aff99e3dae7b3
Kyle Lacy <kylelacy@kyle.space>
2024-04-06 22:27:29 -0700

    Add `project_tags` table to initial DB schema migration

--- migrations/20240406091633_initial_schema.sql
+++ migrations/20240406091633_initial_schema.sql
@@ -3,3 +3,22 @@ CREATE TABLE projects (
     project_json TEXT NOT NULL,
     created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) STRICT;
+
+CREATE TABLE project_tags (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    tag TEXT NOT NULL,
+    project_hash BLOB NOT NULL,
+    is_current BOOLEAN,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX project_tags_name_tag_is_current ON project_tags (name, tag, is_current);
+
+CREATE TRIGGER [project_tags_updated_at]
+    AFTER UPDATE ON project_tags
+    FOR EACH ROW
+BEGIN
+    UPDATE project_tags SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;
+END;
